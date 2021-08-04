dbb515f669d91b03dc4b133adb9fe3b7fe7a86b5
Aman janwani <amanjanwani1486@gmail.com>
2021-08-04 09:35:28 +0530

    CHANGED FIREBASE.JS

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCHnAJ86CwfXXpU5FWqpkpqrXcMtYil0oQ",
-    authDomain: "resume-dcc28.firebaseapp.com",
-    projectId: "resume-dcc28",
-    storageBucket: "resume-dcc28.appspot.com",
-    messagingSenderId: "909398179802",
-    appId: "1:909398179802:web:21b1312d31dae1227b8ae3",
-    measurementId: "G-VV5XS04CB9"
+    apiKey: "API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    projectId: "PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET,
+    messagingSenderId: "MESSAGE_SENDER_ID",
+    appId: "APP_ID",
+    measurementId: "MESASUREMENT_ID"
   };
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
